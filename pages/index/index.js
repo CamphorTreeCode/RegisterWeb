05cc8414a8c874f698f7ee7d143aabe05c41b0d3
LiChengLong <6025242704@qq.com>
2018-03-12 15:14:12 +0800

    图片的提交

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,7 +4,12 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+    movies: [
+      { url: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1520848135396&di=bc802f38c062133311b5dcb02c1dc09c&imgtype=0&src=http%3A%2F%2Fimg.zcool.cn%2Fcommunity%2F0190a359648a67a8012193a366f663.jpg' },
+      { url: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1520848170827&di=2381b4ecf29e18e1c07d577b76786695&imgtype=0&src=http%3A%2F%2Fpic.qiantucdn.com%2F58pic%2F18%2F05%2F28%2F31N58PICg4Z_1024.jpg' },
+      { url: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1520848170827&di=2381b4ecf29e18e1c07d577b76786695&imgtype=0&src=http%3A%2F%2Fpic.qiantucdn.com%2F58pic%2F18%2F05%2F28%2F31N58PICg4Z_1024.jpg' }
+
+    ]    
   },
 
   /**
