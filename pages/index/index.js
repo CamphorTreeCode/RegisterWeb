7f8fd971af8aab6fbee618d9bbca33786b010204
LiChengLong <6025242704@qq.com>
2018-03-12 15:33:38 +0800

    轮播图

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,12 +4,24 @@ Page({
    * 页面的初始数据
    */
   data: {
+    //轮播图
     movies: [
       { url: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1520848135396&di=bc802f38c062133311b5dcb02c1dc09c&imgtype=0&src=http%3A%2F%2Fimg.zcool.cn%2Fcommunity%2F0190a359648a67a8012193a366f663.jpg' },
       { url: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1520848170827&di=2381b4ecf29e18e1c07d577b76786695&imgtype=0&src=http%3A%2F%2Fpic.qiantucdn.com%2F58pic%2F18%2F05%2F28%2F31N58PICg4Z_1024.jpg' },
       { url: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1520848170827&di=2381b4ecf29e18e1c07d577b76786695&imgtype=0&src=http%3A%2F%2Fpic.qiantucdn.com%2F58pic%2F18%2F05%2F28%2F31N58PICg4Z_1024.jpg' }
 
-    ]    
+    ] ,
+    //功能区域  
+    functionImg: [
+      { url:"/img/工商注册@2x.png"},
+      { url:"/img/代理记账@2x.png"},
+      { url: "/img/贷款@2x.png.png" },
+      { url: "/img/网站建站@2x.png" },
+      { url: "/img/商标专利@2x.png" },
+      { url: "/img/宣传推广@2x.png" },
+      { url: "/img/工商注册@2x.png" },
+      { url: "/img/代理记账@2x.png" },
+    ]
   },
 
   /**
